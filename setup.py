8e7c776922930badd961ac24274b4ab45a2bbb15
Greg Albrecht <gba@gregalbrecht.com>
2019-05-30 16:09:46 -0700

    bump

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ import sys
 import setuptools
 
 __title__ = 'cambot'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 __author__ = 'Greg Albrecht <oss@undef.net>'
-__copyright__ = 'Copyright 2017 Greg Albrecht'
+__copyright__ = 'Copyright 2019 Greg Albrecht'
 __license__ = 'Apache License, Version 2.0'
 
 
